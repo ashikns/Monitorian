ec738e4944faad8794153f13c7d21d99ddbc5523
Ashik Salim <ashikns@gmail.com>
2019-07-06 13:00:11 +0530

    replace expressions namespace with new nuget

--- Source/Monitorian.Core/Views/Behaviors/ItemBehavior.cs
+++ Source/Monitorian.Core/Views/Behaviors/ItemBehavior.cs
@@ -1,11 +1,8 @@
-﻿using System;
+﻿using Microsoft.Xaml.Behaviors;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Interactivity;
 
 namespace Monitorian.Core.Views.Behaviors
 {
--- Source/Monitorian.Core/Views/Behaviors/ItemSliderBehavior.cs
+++ Source/Monitorian.Core/Views/Behaviors/ItemSliderBehavior.cs
@@ -1,13 +1,11 @@
-﻿using System;
+﻿using Microsoft.Xaml.Behaviors;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
 using System.Windows.Input;
-using System.Windows.Interactivity;
 
 namespace Monitorian.Core.Views.Behaviors
 {
